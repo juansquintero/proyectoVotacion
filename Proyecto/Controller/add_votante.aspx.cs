53a4b20c1b449ee35c9789fd8796aa243b439aa1
Juan Sebastian Quintero <strangelife28@gmail.com>
2020-10-30 00:50:24 -0500

    Validacion de votante

--- Proyecto/Controller/add_votante.aspx.cs
+++ Proyecto/Controller/add_votante.aspx.cs
@@ -16,8 +16,39 @@ public partial class View_add_votante : System.Web.UI.Page
     protected void button_enviar(object sender, EventArgs e)
     {
         ClientScriptManager cm = this.ClientScript;
-
         E_user user = new E_user();
+
+        string cedula = Page.Request.Form["cedula"].ToString();
+        int validate_cedula = 0;
+        bool comprobation = int.TryParse(cedula, out validate_cedula);
+        if (comprobation == true)
+        {
+            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Funciona perro');</script>");
+            E_user checkUser = new DAO_User().GetVotanteCheck(cedula);
+            if (checkUser.Cedula == cedula)
+            {
+                cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Es usuario ya existe');</script>");
+            }
+            else
+            {
+                todo();
+            }
+        }
+        else
+        {
+            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Digite su cedula');</script>");
+            Response.Redirect("~/View/admin_menu.aspx");
+        }
+
+        
+    }
+
+    protected void todo()
+    {
+        ClientScriptManager cm = this.ClientScript;
+        E_user user = new E_user();
+
+        string cedula = Page.Request.Form["cedula"].ToString();
 
         string user_name = Page.Request.Form["name"].ToString();
         if (string.IsNullOrEmpty(user_name))
@@ -39,21 +70,6 @@ public partial class View_add_votante : System.Web.UI.Page
             user.User_lastname = user_lastname;
         }
 
-        string cedula = Page.Request.Form["cedula"].ToString();
-        int validate_cedula=0;
-        bool comprobation = int.TryParse(cedula, out validate_cedula);
-        if (comprobation==true)
-        {
-            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Funciona perro');</script>");
-            user.Cedula = cedula;
-        }
-        else
-        {
-            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Digite su cedula');</script>");
-            Response.Redirect("~/View/admin_menu.aspx");
-        }
-
-       
         string user_mail = Page.Request.Form["email"].ToString();
         if (string.IsNullOrEmpty(user_mail))
         {
@@ -81,7 +97,7 @@ public partial class View_add_votante : System.Web.UI.Page
         }
         else if (month == 0)
         {
-            
+
             if (day <= 0)
             {
                 year--;
@@ -96,7 +112,7 @@ public partial class View_add_votante : System.Web.UI.Page
         {
             user.Nacimiento = date_nac;
         }
-            //De la linea 555 a 76 se valida la edad por medio de una operación matemática
+        //De la linea 555 a 76 se valida la edad por medio de una operación matemática
 
         string date_exp = Page.Request.Form["date_e"].ToString();
         if (string.IsNullOrEmpty(date_exp))
@@ -108,6 +124,7 @@ public partial class View_add_votante : System.Web.UI.Page
             user.Expe = date_exp;
         }
 
+        user.Cedula = cedula;
         user.Voto = false;
         new DAO_User().save_votantes(user);
         cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Ha funcionado');</script>");
