1ba8f47689aca800fbe32bb090f1e30bff03332e
Juan Sebastian Quintero <strangelife28@gmail.com>
2020-10-30 07:26:57 -0500

    Validacion de administrador

--- Proyecto/Controller/admin_new.aspx.cs
+++ Proyecto/Controller/admin_new.aspx.cs
@@ -17,21 +17,32 @@ public partial class View_admin_new : System.Web.UI.Page
         ClientScriptManager cm = this.ClientScript;
 
         string user_name = Page.Request.Form["username"].ToString();
-        if (string.IsNullOrEmpty(user_name))
+
+        E_admin ps = new DAO_User().getAdminCheck(user_name);
+
+        if (ps == null)
+        {
+            if (string.IsNullOrEmpty(user_name))
+            {
+                cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Ingrese un usuario');</script>");
+            }
+            else
+            {
+                E_admin euser = new E_admin();
+                Random r = new Random();
+
+                euser.User_name_admin = user_name;
+                euser.User_code_admin = r.Next(10000, 99999).ToString();
+
+                new DAO_User().save_admin(euser);
+                //Pendiente correo para confirmar contraseña de admin
+            }
+        }
+        else if (ps.User_name_admin == user_name)
         {
             cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Ingrese un usuario');</script>");
+            Response.Redirect("~/View/admin_new.aspx");
         }
-
-        E_admin euser = new E_admin();
-        Random r = new Random();
-
-        euser.User_name_admin = user_name;
-        euser.User_code_admin = r.Next(10000, 99999).ToString();
-
-        new DAO_User().save_admin(euser);
-
-        //Pendiente correo para confirmar contraseña de admin
-
     }
 
     protected void salirButton(object sender, EventArgs e)
