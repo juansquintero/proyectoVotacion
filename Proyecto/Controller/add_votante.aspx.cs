29812631869f8db2bcecab24180194222394d13e
ANDERSON SNEIDER RINCON SANABRIA <andersonsrincon@ucundinamarca.edu.co>
2020-11-23 16:06:27 -0500

    sexo anal

--- Proyecto/Controller/add_votante.aspx.cs
+++ Proyecto/Controller/add_votante.aspx.cs
@@ -138,7 +138,7 @@ public partial class View_add_votante : System.Web.UI.Page
                             //cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Ingrese su fecha de nacimiento');</script>");
                             //Response.Redirect("~/View/add_votante.aspx");
                         }
-                        else if (year == 18 && month < 1)
+                        else if (year == 18 && month < 1 && month>-1)
                         {
                             ClientScript.RegisterStartupScript(this.GetType(), "Alert", "alert('No es posible');window.open('add_votante.aspx','_self');", true);
                             //cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('Ingrese su fecha de nacimiento');</script>");
