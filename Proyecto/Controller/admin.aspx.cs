a9476c43844ce9156174f89b64f07e72c38cf993
Juan Sebastian Quintero <strangelife28@gmail.com>
2020-11-23 05:54:20 -0500

    Fix login admin

--- Proyecto/Controller/admin.aspx.cs
+++ Proyecto/Controller/admin.aspx.cs
@@ -9,11 +9,7 @@ public partial class View_admin : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        var Logged = Session["validUser"];
-        if (Logged == null)
-        {
-            Response.Redirect("index.aspx");
-        }
+
     }
 
     protected void loginButton(object sender, EventArgs e)
